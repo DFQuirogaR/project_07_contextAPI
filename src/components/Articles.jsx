ff05716e6cca10476d56ed9c9980d71b3c7b69f7
Diego Quiroga <113153053+DFQuirogaR@users.noreply.github.com>
2024-08-02 18:58:15 -0500

    Update Components

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,9 @@
 
 export const Articles = () => {
   return (
-    <div>Articles</div>
+    <div>
+      <h1>Artículos</h1>
+      <p>Esta es la página de Artículos</p>
+    </div>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,9 @@
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Contactos</h1>
+      <p>Esta es la página de Contactos</p>
+    </div>
   )
 }
