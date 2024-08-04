3c253c1bcd96ad090e872fb763792dc591867265
Diego Quiroga <113153053+DFQuirogaR@users.noreply.github.com>
2024-08-04 01:52:12 -0500

    configure login

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react"
+import { Context } from "../context/Context";
 
 export const Contact = () => {
+
+  const nameContact = useContext(Context);
+
   return (
     <div>
       <h1>Contactos</h1>
       <p>Esta es la página de Contactos</p>
+      <p>Estos son los datos de contacto de <strong>{nameContact.phone}</strong></p>
+      <p>Objeto: {JSON.stringify(nameContact)}</p>
     </div>
   )
 }
